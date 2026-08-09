0fb0309c6657de1661e007f0d7f27e0df41f0c0c
vijay1912 <116304203+vijay1912@users.noreply.github.com>
2026-08-09 22:51:40 +0530

    Update ec2_alb_stack.py

--- my_cdk_app/ec2_alb_stack.py
+++ my_cdk_app/ec2_alb_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
+    aws_autoscaling as autoscaling,
     aws_elasticloadbalancingv2 as elbv2,
     Duration
 )
@@ -12,11 +13,11 @@ class Ec2AlbStack(Stack):
         super().__init__(scope, id, **kwargs)
 
         # VPC
-        vpc = ec2.Vpc(self, "CDKmyVpc", max_azs=2)
+        vpc = ec2.Vpc(self, "MyVpc", max_azs=2)
 
         # Security Group
         sg = ec2.SecurityGroup(
-            self, "CDKmySecurityGroup",
+            self, "MySecurityGroup",
             vpc=vpc,
             description="Allow SSH and HTTP",
             allow_all_outbound=True
@@ -24,28 +25,30 @@ class Ec2AlbStack(Stack):
         sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "Allow SSH")
         sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "Allow HTTP")
 
-        # EC2 Instance
-        ec2_instance = ec2.Instance(
-            self, "CDKmyInstance",
+        # Auto Scaling Group
+        asg = autoscaling.AutoScalingGroup(
+            self, "MyASG",
+            vpc=vpc,
             instance_type=ec2.InstanceType("t2.micro"),
             machine_image=ec2.MachineImage.latest_amazon_linux2(),
-            vpc=vpc,
+            min_capacity=1,
+            max_capacity=3,
             security_group=sg,
-            key_pair=ec2.KeyPair.from_key_pair_name(self, "CDKmyKeyPair", "CDKmy-keypair")
+            key_pair=ec2.KeyPair.from_key_pair_name(self, "MyKeyPair", "my-keypair")
         )
 
         # User Data: install Apache
-        ec2_instance.add_user_data(
+        asg.add_user_data(
             "sudo yum update -y",
             "sudo yum install -y httpd",
             "sudo systemctl start httpd",
             "sudo systemctl enable httpd",
-            "echo '<h1>Hello from EC2 behind ALB!</h1>' | sudo tee /var/www/html/index.html"
+            "echo '<h1>Hello from Auto Scaling EC2 behind ALB!</h1>' | sudo tee /var/www/html/index.html"
         )
 
         # Application Load Balancer
         alb = elbv2.ApplicationLoadBalancer(
-            self, "CDKmyALB",
+            self, "MyALB",
             vpc=vpc,
             internet_facing=True,
             security_group=sg
@@ -54,16 +57,5 @@ class Ec2AlbStack(Stack):
         # Listener
         listener = alb.add_listener("Listener", port=80)
 
-        # Target Group (attach EC2 instance)
-        listener.add_targets("AppFleet",
-            port=80,
-            targets=[ec2_instance],
-            health_check=elbv2.HealthCheck(
-                path="/",
-                port="80",
-                protocol=elbv2.Protocol.HTTP,
-                healthy_threshold_count=2,
-                unhealthy_threshold_count=2,
-                interval=Duration.seconds(30)
-            )
-        )
+        # Attach ASG to ALB
+        listener
